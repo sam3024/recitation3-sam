b8e5d1795cac13231ded04da93f4653a85d62fec
tiko <skpissay9@gmail.com>
2024-10-11 23:41:44 -0400

    too

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -6,133 +6,18 @@ import java.util.Objects;
  * @author (Marwan Rasamny)
  * @version (0.1)
  */
-public class Point
-{
-    // fields
+public class Point {
     private double x;
     private double y;
 
-    /**
-     * Constructor - initializes an instance of Point to (0,0).
-     *
-     */
-    public Point()
-    {
-        setX(0);
-        setY(0);
-    }
-
-    /**
-     * Constructor - initializes an instance of Point to (x,y).
-     *
-     */
-    public Point(double x, double y)
-    {
-        setX(x);
-        setY(y);
-    }
-
-    /**
-     * Copy constructor - duplicates the coordinates of aPoint.
-     */
-    public Point(Point aPoint)
-    {
-        x = aPoint.x;
-        y = aPoint.y;
-    }
-
-    // accessor methods
-
-    /**
-     * Returns the value of the x coordinate.
-     */
-    public double getX()
-    {
-        return x;
-    }
-
-    /**
-     * Returns the value of the y coordinate.
-     */
-    public double getY()
-    {
-        return y;
-    }
-
-    /**
-     * Returns the distance from the origin to this point.
-     */
-    public double distanceTo()
-    {
-        return distanceTo(new Point());
-    }
-
-    /**
-     * Returns the distance from this point to aPoint.
-     */
-    public double distanceTo(Point aPoint)
-    {
-        return Math.sqrt((x-aPoint.x)*(x-aPoint.x) +
-                (y-aPoint.y)*(y-aPoint.y));
-    }
-
-    //mutators
-
-    /**
-     * Sets the x coordinate to the value of x.
-     *
-     * @param x value of the x coordinate to set.
-     */
-    public void setX(double x)
-    {
+    // Constructor
+    public Point(double x, double y) {
         this.x = x;
-    }
-
-    /**
-     * Sets the y coordinate to the value of y.
-     *
-     * @param y value of the y coordinate to set.
-     */
-    public void setY(double y)
-    {
         this.y = y;
     }
 
-    /**
-     * Changes the x coordinate by dx and the y by dy.
-     *
-     * @param dx The value by which to change x.
-     * @param dy The value by which to change y.
-     */
-    public void translate(double dx, double dy)
-    {
-        x += dx;
-        y += dy;
-    }
-
-    /**
-     * Returns the string representation of this point.
-     */
-    @Override
-    public String toString()
-    {
-        return "("+x+","+y+")";
-    }
-
-    /**
-     * Returns true if an only if o is an instance of Point and o's x and y
-     * values are equal to this Point object's x and y values
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
+    // Method to calculate the distance between this point and another point
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
     }
 }
--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class Recitation3 {
-    public static void main(String[] argv)
-    {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // add your code here
+        // Get x and y coordinates from the user
+        System.out.println("Enter x:");
+        double x = scanner.nextDouble();
+        System.out.println("Enter y:");
+        double y = scanner.nextDouble();
+
+        // Create an instance of Circle at (x,y) with radius 16.0
+        Circle circle16 = new Circle(new Point(x, y), 16.0);
+        // Create another instance of Circle at (14,12) with radius 40.0
+        Circle circle40 = new Circle(new Point(14, 12), 40.0);
+
+        // Print whether the circles intersect
+        System.out.println(circle16.intersects(circle40));
+
+        scanner.close();
     }
 }
